60610a9caba0683e52f2cd39061f959eba873a90
General <rlfosterjr@gmail.com>
2017-05-17 09:57:32 -0400

    Update consolas.sh

--- consolas.sh
+++ consolas.sh
@@ -7,8 +7,10 @@
 
 set -e
 set -x
-mkdir temp
-cd temp
+cd ~/Downloads
+mkdir consolas
+cd consolas
 wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
 cabextract -L -F ppviewer.cab PowerPointViewer.exe
 cabextract ppviewer.cab
+open CONSOLA*.TTF
